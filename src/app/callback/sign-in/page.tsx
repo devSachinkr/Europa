c781f73687c787c18597dd7a0c946fed4dda5c7e
Sachin kumar <sachin67.dev@gmail.com>
2025-01-06 18:45:45 +0530

    Update sign-in callback page and authentication hooks

--- src/app/callback/sign-in/page.tsx
+++ src/app/callback/sign-in/page.tsx
@@ -1,7 +1,6 @@
 import { signInUser } from "@/actions/auth";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import React from "react";
 
 const page = async () => {
     const user = await currentUser();
