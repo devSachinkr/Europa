fc1088be6d2e7ce71d58f1ff7d9f296562186ff8
Sachin kumar <sachin67.dev@gmail.com>
2025-01-06 14:44:03 +0530

    Update content and functionality for group creation page

--- src/app/group/create/page.tsx
+++ src/app/group/create/page.tsx
@@ -6,55 +6,50 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
-    searchParams: {
-        [affiliate: string]: string;
-    };
-}
+  searchParams: Promise<{ [affiliate: string]: string }>;
+};
 
-const page = async (props: Props) => {
-    const {
-      searchParams: { affiliate },
-    } = props;
-    const user = await authUser();
-    const affiliateInfo = await getAffiliateInfo({ affiliate });
-    if (!user || !user.id) return redirect("/sign-in");
+const page = async ({ searchParams }: Props) => {
+  const { affiliate } = await searchParams;
+  const user = await authUser();
+  const affiliateInfo = await getAffiliateInfo({ affiliate });
+  if (!user || !user.id) return redirect("/sign-in");
 
-    return (
-        <>
-            <div className="px-7 flex flex-col">
-                <h5 className="font-bold text-base text-themeTextWhite">
-                    Payment Method
-                </h5>
-                <p className="text-themeTextGray leading-tight">
-                    We are a community-driven platform that empowers users to
-                    connect with like-minded individuals from around the world.
-                    Whether {"you're"} a seasoned traveler, a curious explorer,
-                    or simply seeking new adventures, Europa offers a diverse
-                    range of experiences that will inspire and enrich your
-                    journey.
-                </p>
-                {affiliateInfo.status === 200 && (
-                    <div className="w-full mt-5 flex justify-center items-center gap-x-2 italic text-themeTextGray text-sm">
-                        You were referred by{" "}
-                        <Avatar>
-                            <AvatarImage
-                                src={affiliateInfo.data?.Group?.User?.image as string}
-                                alt="User"
-                            />
-                            <AvatarFallback>US</AvatarFallback>
-                        </Avatar>
-                        {affiliateInfo.data?.Group?.User.firstName}{" "}
-                        {affiliateInfo.data?.Group?.User.lastName}
-                    </div>
-                )}
-            </div>
-            <GroupForm
-                userID={user.id}
-                affiliate={affiliateInfo.status === 200}
-                stripeId={affiliateInfo.data?.Group?.User.stripeId || ""}
-            />
-        </>
-    );
+  return (
+    <>
+      <div className="px-7 flex flex-col">
+        <h5 className="font-bold text-base text-themeTextWhite">
+          Payment Method
+        </h5>
+        <p className="text-themeTextGray leading-tight">
+          We are a community-driven platform that empowers users to connect with
+          like-minded individuals from around the world. Whether {"you're"} a
+          seasoned traveler, a curious explorer, or simply seeking new
+          adventures, Europa offers a diverse range of experiences that will
+          inspire and enrich your journey.
+        </p>
+        {affiliateInfo.status === 200 && (
+          <div className="w-full mt-5 flex justify-center items-center gap-x-2 italic text-themeTextGray text-sm">
+            You were referred by{" "}
+            <Avatar>
+              <AvatarImage
+                src={affiliateInfo.data?.Group?.User?.image as string}
+                alt="User"
+              />
+              <AvatarFallback>US</AvatarFallback>
+            </Avatar>
+            {affiliateInfo.data?.Group?.User.firstName}{" "}
+            {affiliateInfo.data?.Group?.User.lastName}
+          </div>
+        )}
+      </div>
+      <GroupForm
+        userID={user.id}
+        affiliate={affiliateInfo.status === 200}
+        stripeId={affiliateInfo.data?.Group?.User.stripeId || ""}
+      />
+    </>
+  );
 };
 
 export default page;
